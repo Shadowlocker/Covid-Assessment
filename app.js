0445c5a06053683aa0f81e3b79ee4f8741a9978f
rinkyfulwani <rinky.fulwani@yahoo.in>
2021-10-14 15:05:30 -0700

    Node js files

--- app.js
+++ app.js
@@ -33,4 +33,12 @@ app.listen("3000", function() {
 
 app.get("/", function(req, res) {
   res.render("home");
-})
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact");
+});
