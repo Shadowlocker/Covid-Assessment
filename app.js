a94e5158c852f1ba24c25b88310b103d194b143a
sujatha <sujatce@gmail.com>
2021-10-20 13:47:08 -0700

    database insert

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var mysql = require('mysql');
 const app = express();
 
 //catest@1234
-/*
-var con = mysql.createConnection({
+
+/*var con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "catest@1234"
@@ -21,8 +21,24 @@ con.connect(function(err) {
   //   console.log("Database created");
   // });
 });
-
 */
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "suja28@TCS"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  //  con.query("create database covid_assessment_db_instance", function (err, result) {
+  //   if (err) throw err;
+  //   console.log("Database created");
+  //  });
+});
+
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -45,5 +61,28 @@ app.get("/contact", function(req, res) {
 });
 app.get("/signup", function(req, res) {
   res.render("signup");
+});
+
+app.post("/success", function(req, res) {
+  var user = {
+
+  }
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var birthdayDate = req.body.birthdayDate;
+  var Gender = req.body.inlineRadioOptions;
+  var Address = req.body.Address;
+  var emailAddress = req.body.emailAddress;
+  var password = req.body.password;
+  
+    var sql = "INSERT INTO users(first_name, last_name, dob, gender, address, email, password) VALUES (?,?,?, ?, ?, ?, ?)";
+    con.query("use covid_assessment_db_instance", function(err, result) {
+
+    });
+    con.query(sql, [firstName, lastName, birthdayDate, Gender, Address, emailAddress, password], function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted"+result.insertedId);
+    });
+    res.render("success");
 });
 
