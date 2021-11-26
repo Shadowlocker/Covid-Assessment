002f5e04ab4664d6aa059f32656fa707d388764d
rinkyfulwani <rinky.fulwani@yahoo.in>
2021-11-26 02:27:01 -0800

    Added signout changes

--- app.js
+++ app.js
@@ -282,3 +282,16 @@ app.post("/reschedule", function(req, res) {
 
 });
 });
+
+app.get('/signOut',  function (req, res, next)  {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function (err) {
+      if (err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+});
